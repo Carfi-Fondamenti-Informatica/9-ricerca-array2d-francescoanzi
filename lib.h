b2c507a82b2145aa6bc83c9e52d63769abb8e9fe
francescoanzi <92693084+francescoanzi@users.noreply.github.com>
2021-12-11 22:53:19 +0100

    Add files via upload

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by oelsp on 11/12/2021.
+//
 
+#ifndef ESERCIZIO_VALUTATO_9_LIB_H
+#define ESERCIZIO_VALUTATO_9_LIB_H
+
+bool ricerca(char lista [10][20], char nome [1][20], int &pos);
+
+#endif //ESERCIZIO_VALUTATO_9_LIB_H
